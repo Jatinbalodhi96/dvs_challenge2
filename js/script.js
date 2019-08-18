4d29e2729b1690b1feedd49864a772ce7dcc208e
Jatin Balodhi <jatin.balodhi@gramener.com>
2019-08-18 06:07:37 +0530

    Added word cloud

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
-$(document).ready(app)
+$(window).ready(app)
+
+var data;
 
 function app() {
+    render_table()
+    render_word_cloud()
+}
+
+function render_table() {
     $.ajax({
         'method': 'GET',
         'url': 'assets/yearly_pay.json',
-        'async': false,
+        // 'async': false,
         'success': function(resp) {
-            render_table(resp)
+            $('#table-container').on('template', function() {
+                $('[data-toggle="tooltip"]').tooltip();
+            }).template({'data': resp})
         }
-    })
+    });
+
 }
 
-function render_table(data) {
-    $('#table-container').on('template', function() {
-        $('[data-toggle="tooltip"]').tooltip();
-    }).template({'data': data})
+function render_word_cloud() {
+    $.ajax({
+        'method': 'GET',
+        'url': 'assets/dashboard.json',
+        'async': false,
+        'success': function(resp) {
+            data = resp['education_vs_tools']['Equal Parts School and Self-Taught']
+            wordCloud('', data)
+        }
+    });
+    // d3.json('assets/dashboard.json').then(function(data) {
+    //     console.log(data)
+    // })
 }
 
 var table_color = d3.scaleQuantile()
